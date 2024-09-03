b12457f538a1e68918363a0e25dbb7435011cc9a
eddie-m-m <eddie.m.menefee@gmail.com>
2024-09-03 08:09:34 -0700

    Barebones html printer

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn myhtml
+
+myhtml = makeHtml "My page title" "My page content"
+
+makeHtml title body = html_ (head_ (title_ title) <> body_ body)
+
+html_ content = "<html>" <> content <> "</html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
