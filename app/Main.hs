74620094dd30652d0a29e835854a8127b86404f7
eddie-m-m <eddie.m.menefee@gmail.com>
2024-09-03 08:52:58 -0700

    Add generic el to wrap all tags, add some more specific tags

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,35 @@
 module Main where
 
+main :: IO ()
 main = putStrLn myhtml
 
-myhtml = makeHtml "My page title" "My page content"
+myhtml :: String
+myhtml =
+  makeHtml
+    "Refugium, remix"
+    (h1_ "Reflections")
+    <> (p_ "Reflection 1" <> p_ "Reflection 2")
 
+makeHtml :: String -> String -> String
 makeHtml title body = html_ (head_ (title_ title) <> body_ body)
 
-html_ content = "<html>" <> content <> "</html>"
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-body_ content = "<body>" <> content <> "</body>"
+html_ :: String -> String
+html_ = el "html"
 
-head_ content = "<head>" <> content <> "</head>"
+body_ :: String -> String
+body_ = el "body"
 
-title_ content = "<title>" <> content <> "</title>"
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
